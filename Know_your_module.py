9441e73e48cd86985a7d6cc22646272edf12489b
Prince Goyal <prince2k5@users.noreply.github.com>
2022-06-19 16:03:28 +0530

    Update Know_your_module.py
    Signed-off-by: Prince Goyal <prince2k5@users.noreply.github.com>

--- Know_your_module.py
+++ Know_your_module.py
@@ -18,27 +18,28 @@ def explain(m):
         return f"{m[0]}(???)"  # some functions don't provide a signature
 
 if __name__=='__main__':
-    modulename = st.text_input('Please enter name of the module')
-    if modulename is not None:
+    try:
+        modulename = st.text_input('Please enter name of the module')
         if modulename is not None:
-            try:
-                mod_name = importlib.import_module(modulename)
-                m = {k for k in dir(mod_name) if not k.startswith('_')}
-                a = {m[0]: signature(m[1]) for m in getmembers(mod_name, isfunction)}
-                st.table(pd.Series(a))
+            if modulename is not None:
+                try:
+                    mod_name = importlib.import_module(modulename)
+                    m = {k for k in dir(mod_name) if not k.startswith('_')}
+                    a = {m[0]: signature(m[1]) for m in getmembers(mod_name, isfunction)}
+                    st.table(pd.Series(a))
 
-                if m is not None:
-                    with st.expander('Module properties'):
-                        selected=st.selectbox("Module Properties",m)
-                        if selected is not None:
-                            try:
-                                st.code(signature(mod_name.__dict__[selected]))
-                            except:
-                                st.table(getmembers(mod_name.__dict__[selected]))
+                    if m is not None:
+                        with st.expander('Module properties'):
+                            selected=st.selectbox("Module Properties",m)
+                            if selected is not None:
+                                try:
+                                    st.code(signature(mod_name.__dict__[selected]))
+                                except:
+                                    st.table(getmembers(mod_name.__dict__[selected]))
 
-            except Exception as err:
-                st.exception(err)
-
+                except Exception as err:
+                    st.exception(err)
+    except:
         with st.expander('SQL'):
             img=Image.open(r"C:\Users\princ\Downloads\sqlcheatsheet.png")
             st.image(img)
